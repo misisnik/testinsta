58c1049f2767f612e8be7cee2ad2ad3d9ea49371
Michal Sladecek <misisnik@gmail.com>
2017-08-15 14:19:32 +0200

    needs flask?

--- examples/like_and_follow_last_user_media_likers.py
+++ examples/like_and_follow_last_user_media_likers.py
@@ -13,8 +13,11 @@ from os import environ
 from flask import Flask
 
 app = Flask(__name__)
-app.run()
 
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 sys.path.append(os.path.join(sys.path[0], '../'))
 from instabot import Bot
